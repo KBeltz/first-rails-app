3972ddcc3296279447f56e22e217d482b3793b3b
Kristin Beltz <beltz08@me.com>
2015-07-21 22:33:52 -0500

    First route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    # loads view from "views/users/index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+get "users/list" => 'users#index'
+
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
